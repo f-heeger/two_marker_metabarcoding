c02c86f613acd0783ae7a49d5abcf7e4abf2fa8c
heeger <fheeger@mi.fu-berlin.de>
2017-11-30 13:00:52 +0100

    removed lca function from prepDatabase file (it can use the one from the main file)

--- prepDatabases.snakefile.py
+++ prepDatabases.snakefile.py
@@ -161,60 +161,6 @@ rule db_creatUniteIndex:
     shell:
         "%(lambdaFolder)s/lambda_indexer -d {input} -p blastn -t {threads}" % config
 
-def lca(lineageStrings, stringency=1.0, 
-        unidentified=["unidentified", "unclassified", "unknown"],
-        ignoreIncertaeSedis=True, sizes=None):
-    lineage = []
-    mLineages = []
-    #remove bootstrap values ("(100)", "(75)", etc.) if any
-    for mLin in [l.strip(";").split(";") for l in lineageStrings]:
-        mLineages.append([])
-        for entry in mLin:
-             mLineages[-1].append(entry.split("(")[0])
-    maxLinLen = max([len(m) for m in mLineages])
-    active = [True]*len(mLineages)
-    for i in range(maxLinLen):
-        total = 0.0
-        counts = {}
-        for m, memberLin in enumerate(mLineages):
-            if not active[m]:
-                continue #ignore lineages that were deactivated further up in the tree
-            if len(memberLin) <= i:
-                active[m] = False
-                continue #ignore lineages that are not this long
-            name = memberLin[i]
-            if name in unidentified:
-                continue # ignoring unidentified entrys
-            if ignoreIncertaeSedis and name.startswith("Incertae"):
-                continue # ignoring Incertae sedis entries.
-                         # NOTE: this will mean lineages end at the first Incerta sedis
-            if sizes is None:
-                tSize = 1
-            else:
-                tSize = sizes[m]
-            total += tSize
-            try:
-                counts[memberLin[i]] += tSize
-            except KeyError:
-                counts[memberLin[i]] = tSize
-        if not counts:
-            #no valid lineage entrys found in this level
-            break
-        most=sorted(counts.items(), key=lambda x: x[1], reverse=True)[0]
-        different = total - most[1]
-        #accept the lineage entry if its proportion of all (valid) classifications higher than stringency setting
-        if different/total <= (1.0-stringency):
-            lineage.append(most[0]) #add the most apearing entry to the new lineage
-            #deactivate all lineages that were different at this level
-            for m, memberLin in enumerate(mLineages):
-                if active[m] and memberLin[i] != most[0]:
-                    active[m] = False
-        else:
-            break
-    if len(lineage) == 0:
-        lineage = ["unknown"]
-    return ";".join(lineage)
-
 
 class NcbiMap(dict):
     """Base class for a dictonary relying on the BioPython NCBI interface for 
