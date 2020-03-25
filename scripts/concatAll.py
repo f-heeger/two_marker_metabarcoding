c3b72af312a67e56dbc7886f52dc0e1a96e8ec6d
Felix Heeger <fheeger@mi.fu-berlin.de>
2020-03-25 14:34:36 +0100

    fixed more bugs in concat script

--- scripts/concatAll.py
+++ scripts/concatAll.py
@@ -6,8 +6,8 @@ with gzip.open(snakemake.output.comb, "wt") as combOut, \
      open(snakemake.output.sample, "w") as sampleOut, \
      open(snakemake.output.name, "w") as nameOut:
     for sample, fileInfo in snakemake.params.files.items():
-        for inFile in fileInfo[int(snakemake.wildcards.read)]:
-            with gzip.open(inFile, "rt") as inStream:
+        for inFile in fileInfo[int(snakemake.wildcards.read[1])]:
+            with gzip.open("%s/%s" % (snakemake.config["inFolder"], inFile), "rt") as inStream:
                 for rec in SeqIO.parse(inStream, "fastq"):
                     newId = "_".join(rec.id.split(":")[3:])
                     nameOut.write("%s\t%s\n" % (rec.id, newId))
