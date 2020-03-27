bf33802292e3d48ed7ed4206cef94577cb387972
Felix Heeger <fheeger@mi.fu-berlin.de>
2020-03-27 09:47:49 +0100

    fixed problem in concat read selection

--- scripts/concatAll.py
+++ scripts/concatAll.py
@@ -6,7 +6,8 @@ with gzip.open(snakemake.output.comb, "wt") as combOut, \
      open(snakemake.output.sample, "w") as sampleOut, \
      open(snakemake.output.name, "w") as nameOut:
     for sample, fileInfo in snakemake.params.files.items():
-        for inFile in fileInfo[int(snakemake.wildcards.read[1])]:
+        #print(fileInfo)
+        for inFile in fileInfo[int(snakemake.wildcards.read[1])-1]:
             with gzip.open("%s/%s" % (snakemake.config["inFolder"], inFile), "rt") as inStream:
                 for rec in SeqIO.parse(inStream, "fastq"):
                     newId = "_".join(rec.id.split(":")[3:])
