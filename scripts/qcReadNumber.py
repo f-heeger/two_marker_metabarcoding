fed39b3803dc3b8245677ac435394b0291c1e228
Felix Heeger <fheeger@mi.fu-berlin.de>
2021-04-20 16:34:40 +0200

    fixed mor eproblems with file naming

--- scripts/qcReadNumber.py
+++ scripts/qcReadNumber.py
@@ -1,19 +1,23 @@
 import json
 
 file2sample = {}
+file2readNum = {}
 for sample, fileInfo in snakemake.params.files.items():
     for fileName in fileInfo[0]:
         file2sample[fileName.rsplit(".", 2)[0]] = sample
+
+
+
 
 seqNum = {}
 with open(snakemake.input[0]) as inStream:
     data = json.load(inStream)
     for fileName, sData in data["report_general_stats_data"][0].items():
-        libName, lane, readNum, part = fileName.rsplit("_", 3)
-        if readNum != "R1":
-            continue # only use R1
+        try:
+            sample = file2sample[fileName]
+        except KeyError:
+            continue # files not found here are probably for read 2
         n = int(sData["total_sequences"])
-        sample = file2sample[fileName]
         try:
             seqNum[sample] += n
         except KeyError:
